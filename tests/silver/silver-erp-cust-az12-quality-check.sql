9cee965f6eb28e635d54a4a2a3c76e4e0e679e66
Andrii L <riiand.develop@gmail.com>
2025-11-11 14:58:22 -0700

    enh(DWH-25): Add quality check scripts for customer, product, and sales dimensions in Silver Layer

---
+++ tests/silver/silver-erp-cust-az12-quality-check.sql
@@ -0,0 +1,26 @@
+--- Check For Nulls or Duplicates in Primary Key
+--- Expectation: No Result
+
+SELECT cst_id, COUNT(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL
+
+-- Check for unwanted Spaces
+--- Expectation: No Result
+-- Firstname
+SELECT cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+-- Lastname
+SELECT cst_lastname
+FROM silver.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname)
+
+-- Data Sandartization & Consistancy
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info
+
+SELECT DISTINCT cst_marital_status
+FROM silver.crm_cust_info
